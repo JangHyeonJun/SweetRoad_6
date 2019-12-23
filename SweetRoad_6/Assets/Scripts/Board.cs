14d422dd0d1faa6006979c371cdb1f4b47100bcb
JangHyeonjun <totok682@naver.com>
2019-12-23 19:08:44 +0900

    블록 이동 구현 완료

--- SweetRoad_6/Assets/Scripts/Board.cs
+++ SweetRoad_6/Assets/Scripts/Board.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
+    public enum Array { UP, DOWN, LEFT, RIGHT, NONE };
     const int board_size = 7;
-    public GameObject tile;
-    public GameObject[,] tiles;
+    public GameObject origin_tile;
     public List<Sprite> tile_sprites;
     public Transform offset;
+
+    private GameObject[,] tiles;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +24,57 @@ public class Board : MonoBehaviour
 
     }
 
+    public bool MoveTile(int x, int y, Array array)
+    {
+        int dx, dy;
+        switch (array)
+        {
+            case Array.UP:
+                dx = x; dy = y + 1;
+                break;
+            case Array.DOWN:
+                dx = x; dy = y - 1;
+                break;
+            case Array.LEFT:
+                dx = x - 1; dy = y;
+                break;
+            case Array.RIGHT:
+                dx = x + 1; dy = y;
+                break;
+            default:
+                dx = x; dy = y;
+                break;
+        }
+        if (dy >= 0 && dx >= 0 && dy < board_size && dx < board_size)
+        {
+            Sprite temp = tiles[y, x].GetComponent<SpriteRenderer>().sprite;
+            tiles[y, x].GetComponent<SpriteRenderer>().sprite = tiles[dy, dx].GetComponent<SpriteRenderer>().sprite;
+            tiles[dy, dx].GetComponent<SpriteRenderer>().sprite = temp;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public bool matched(GameObject tile, Array array)
+    {
+        switch (array)
+        {
+            case Array.UP:
+                break;
+            case Array.DOWN:
+                break;
+            case Array.LEFT:
+                break;
+            case Array.RIGHT:
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
     private void CreateBoard()
     {
         tiles = new GameObject[board_size, board_size];
@@ -29,8 +82,8 @@ public class Board : MonoBehaviour
         {
             for (int j = 0; j < board_size; j++)
             {
-                Vector2 render_size = tile.GetComponent<Renderer>().bounds.size;
-                GameObject new_tile = Instantiate(tile, new Vector3(offset.position.x + render_size.x * j,
+                Vector2 render_size = origin_tile.GetComponent<Renderer>().bounds.size;
+                GameObject new_tile = Instantiate(origin_tile, new Vector3(offset.position.x + render_size.x * j,
                     offset.position.y + render_size.y * i, 0), transform.rotation);
 
                 List<Sprite> non_repeated_sprites = new List<Sprite>(tile_sprites);
@@ -41,6 +94,7 @@ public class Board : MonoBehaviour
 
                 new_tile.GetComponent<SpriteRenderer>().sprite = non_repeated_sprites[Random.Range(0, non_repeated_sprites.Count)];
                 new_tile.transform.parent = transform;
+                new_tile.GetComponent<Tile>().SetIndex(j, i);
                 tiles[i, j] = new_tile;
             }
         }
--- SweetRoad_6/Assets/Scripts/Tile.cs
+++ SweetRoad_6/Assets/Scripts/Tile.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-
-    // Start is called before the first frame update
+    private int tile_index_x, tile_index_y;
+    private bool clicked;
+    public float mouse_sensitivity = 10.0f;
+    Vector2 mouse_offset;
+    public void SetIndex(int x, int y)
+    {
+        tile_index_x = x;
+        tile_index_y = y;
+    }
     void Start()
     {
-        
+        clicked = false;
     }
 
     // Update is called once per frame
@@ -16,4 +23,36 @@ public class Tile : MonoBehaviour
     {
         
     }
+
+    private void OnMouseDown()
+    {
+        clicked = true;
+        mouse_offset = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+    }
+
+    private void OnMouseDrag()
+    {
+        float dx = Input.mousePosition.x - mouse_offset.x;
+        float dy = Input.mousePosition.y - mouse_offset.y;
+
+        if (clicked)
+        {
+            Board.Array drag_array = Board.Array.NONE;
+            if (dy > mouse_sensitivity)
+                drag_array = Board.Array.UP;
+            else if (dy < -mouse_sensitivity)
+                drag_array = Board.Array.DOWN;
+            else if (dx < -mouse_sensitivity)
+                drag_array = Board.Array.LEFT;
+            else if (dx > mouse_sensitivity)
+                drag_array = Board.Array.RIGHT;
+
+            if (drag_array != Board.Array.NONE)
+            {
+                clicked = false;
+                GetComponentInParent<Board>().MoveTile(tile_index_x, tile_index_y, drag_array);
+            }
+        }
+    }
+
 }
