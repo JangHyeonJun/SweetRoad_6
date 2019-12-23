015261586f6b9fbfd109ea70e89426982b541622
JangHyeonjun <totok682@naver.com>
2019-12-23 18:09:53 +0900

    블록 초기화 구현 완료

---
+++ SweetRoad_6/Assets/Scripts/Board.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    const int board_size = 7;
+    public GameObject tile;
+    public GameObject[,] tiles;
+    public List<Sprite> tile_sprites;
+    public Transform offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateBoard();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void CreateBoard()
+    {
+        tiles = new GameObject[board_size, board_size];
+        for (int i = 0; i < board_size; i++)
+        {
+            for (int j = 0; j < board_size; j++)
+            {
+                Vector2 render_size = tile.GetComponent<Renderer>().bounds.size;
+                GameObject new_tile = Instantiate(tile, new Vector3(offset.position.x + render_size.x * j,
+                    offset.position.y + render_size.y * i, 0), transform.rotation);
+
+                List<Sprite> non_repeated_sprites = new List<Sprite>(tile_sprites);
+                if (j - 2 > 0)
+                    non_repeated_sprites.Remove(tiles[i, j - 2].GetComponent<SpriteRenderer>().sprite);
+                if (i - 2 > 0)
+                    non_repeated_sprites.Remove(tiles[i - 2, j].GetComponent<SpriteRenderer>().sprite);
+
+                new_tile.GetComponent<SpriteRenderer>().sprite = non_repeated_sprites[Random.Range(0, non_repeated_sprites.Count)];
+                new_tile.transform.parent = transform;
+                tiles[i, j] = new_tile;
+            }
+        }
+    }
+
+}
